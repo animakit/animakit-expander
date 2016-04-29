faf0fd8e5adbd02c91e5d83cc8e8e7ef0ac75d97
Anna Selezniova <ask.dev@gmail.com>
2016-04-29 14:12:00 +0300

    Remove render

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
 import { AnimakitExpander } from './AnimakitExpander';
 
-import React      from 'react';
-import { render } from 'react-dom';
-
-if (typeof document !== 'undefined') {
-  render(<AnimakitExpander/>, document.body);
-}
-
 export default AnimakitExpander;
