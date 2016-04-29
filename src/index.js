2ec9b61e1606d652d5a43ddc010b2d24c396e9e8
Anna Selezniova <ask.dev@gmail.com>
2016-04-29 13:35:21 +0300

    Remove uglifier. Add render. Add new keyword

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import { AnimakitExpander } from './AnimakitExpander';
 
+import React      from 'react';
+import { render } from 'react-dom';
+
+if (typeof document !== 'undefined') {
+  render(<AnimakitExpander/>, document.body);
+}
+
 export default AnimakitExpander;
